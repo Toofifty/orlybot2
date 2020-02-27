a5178fb166a33c68760633839d9035165b6a037a
Toofifty <alex@matho.me>
2020-02-27 12:16:42 +1100

    cleverbot values

--- src/modules/cleverbot.ts
+++ src/modules/cleverbot.ts
@@ -13,5 +13,26 @@ if (process.env.CLEVERBOT_KEY) {
         })
             .desc('Mention me to have a conversation')
             .isPhrase();
+
+        Command.create('set-emotion', (message, [amount]) => {
+            cleverbot.setEmotion(Number(amount));
+            return cleverbot.say("I've set your emotion to ${amount}");
+        })
+            .desc("Set @mathobot's emotion response value")
+            .arg({ name: 'value', required: true });
+
+        Command.create('set-engagement', (message, [amount]) => {
+            cleverbot.setEngagement(Number(amount));
+            return cleverbot.say("I've set your engagement to ${amount}");
+        })
+            .desc("Set @mathobot's engagement response value")
+            .arg({ name: 'value', required: true });
+
+        Command.create('set-regard', (message, [amount]) => {
+            cleverbot.setRegard(Number(amount));
+            return cleverbot.say("I've set your regard to ${amount}");
+        })
+            .desc("Set @mathobot's regard response value")
+            .arg({ name: 'value', required: true });
     });
 }
