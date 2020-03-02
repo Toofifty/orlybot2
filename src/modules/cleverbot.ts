d761fe1c1a5c58e3671f97a9ff214604728dfb29
Toofifty <alex@matho.me>
2020-03-02 12:02:38 +1100

    resolve user tags in cleverbot

--- src/modules/cleverbot.ts
+++ src/modules/cleverbot.ts
@@ -1,14 +1,28 @@
 import { Cleverbot } from 'clevertype';
 import bot from 'core/bot';
 import { Command } from 'core/commands';
+import User from 'core/model/user';
+import { loginfo } from 'core/log';
 
 if (process.env.CLEVERBOT_KEY) {
     const cleverbot = new Cleverbot(process.env.CLEVERBOT_KEY);
 
     bot.ready(({ id }) => {
-        console.log('create command', `<@${id}>`);
-        Command.create(`<@${id}>`, message => {
-            const msg = message.text.replace(`<@${id}>`, 'Botty');
+        Command.create(`<@${id}>`, async message => {
+            let msg = message.text.replace(`<@${id}>`, 'Botty');
+            // resolve users in text
+            const userTags = msg.match(/<@.+?>/g) ?? [];
+
+            const replaceUsers = await Promise.all(
+                userTags.map(async tag => [tag, await User.find(tag)] as const)
+            );
+
+            msg = replaceUsers.reduce((carry, [tag, user]) => {
+                return carry.replace(tag, user.profile.displayName);
+            }, msg);
+
+            loginfo('Resolved message:', msg);
+
             return cleverbot.say(msg);
         })
             .desc('Mention me to have a conversation')
