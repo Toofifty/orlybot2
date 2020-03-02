af39f486ce44f616c57ea2adebacdabbcbad87e6
Toofifty <alex@matho.me>
2020-03-02 20:27:53 +1100

    fix findOption

--- src/modules/lunch/util.ts
+++ src/modules/lunch/util.ts
@@ -1,11 +1,13 @@
 import { LunchOption } from './types';
 
 export const findOption = (options: LunchOption[], name: string) =>
-    options.find(option => option.name.toLowerCase() === name.toLowerCase());
+    options.find(option =>
+        option.name.toLowerCase().includes(name.toLowerCase())
+    );
 
 export const findOptionIndex = (options: LunchOption[], name: string) =>
-    options.findIndex(
-        option => option.name.toLowerCase() === name.toLowerCase()
+    options.findIndex(option =>
+        option.name.toLowerCase().includes(name.toLowerCase())
     );
 
 export const print = ({ name, icon }: LunchOption) => `*${name}* ${icon}`;
