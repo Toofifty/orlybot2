ce5aee37d51eba58dc6fcb440d57ce213da47139
Toofifty <alex@matho.me>
2021-04-20 19:15:25 +1000

    more stuff wip

--- src/modules/new_trivia/trivia.controller.ts
+++ src/modules/new_trivia/trivia.controller.ts
@@ -8,14 +8,17 @@ import {
     required,
     validate,
     delegate,
+    def,
 } from 'core/new';
 import TriviaCategories from './categories.controller';
 import v from './trivia.validators';
+import TriviaStore, { ITriviaStore } from './trivia.store';
 
 @keyword('trivia')
-class Trivia extends CommandController {
+class Trivia extends CommandController<ITriviaStore> {
     async before() {
         await this.message.addReaction('eyes');
+        this.setStore(TriviaStore.make(this));
     }
 
     async after() {
@@ -26,18 +29,23 @@ class Trivia extends CommandController {
     @alias('t')
     async main(
         @arg('difficulty')
+        @def('easy')
         @validate(v.ValidDifficulty)
-        @required
         difficulty = 'easy'
     ) {
-        this.message.reply('I guess it works??');
+        this.store.this.message.reply('I guess it works??');
     }
 
     @sub('Cancel broken trivia')
     cancel() {}
 
     @sub("Get a user's trivia score")
-    score() {}
+    score(
+        @arg('user')
+        @def('@me')
+        @validate(v.UserExists)
+        user = this.message.user.id
+    ) {}
 
     @sub('Check out the trivia leaderboard')
     leaderboard() {}
